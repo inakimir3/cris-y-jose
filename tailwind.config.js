1584dce12888a102dfd3bd9fbbb089c454258fd8
Iñaki Mir <imirbelloso@gmail.com>
2025-09-02 12:26:34 +0200

    Inicio proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FBF8F1',
+        secondary: '#C3937C',
+        accent: '#787878',
+      },
+      fontFamily: {
+        inter: ['Inter Variable'],
+        garamond: ['EB Garamond Variable'],
+        cormorant: ['Cormorant Garamond Variable'],
+        infant: ['Cormorant Infant Variable'],
+      },
+    },
+  },
+  plugins: [],
+}
+
